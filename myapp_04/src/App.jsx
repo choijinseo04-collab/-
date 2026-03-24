7e523c301701ebb48176cdce02b14581d7bc722d
Jinseo Choi <choijinseo04@gmail.com>
2026-03-24 12:21:18 +0900

    최종0324

--- myapp_04/src/App.jsx
+++ myapp_04/src/App.jsx
@@ -29,8 +29,10 @@ function Id({ name, age, job }) {
   );
 }
 
-function Button({ text }) {
-  return <button>{text}</button>;
+function Button({ text, color }) { // 색 넣는거 추가
+  return (
+    <button style={{ backgroundColor: color }}>{text}</button>
+  );
 }
 
 function Box({ children }) {
@@ -51,8 +53,65 @@ function FruitList({ fruits }) {
   );
 }
 
+function Book(props) {
+  return (
+    <div>
+      <h4>이 책의 제목은 {props.title} 입니다.</h4>
+      <p>이 책의 저자는 {props.author} 입니다.</p>
+      <p>이 책의 가격은 {props.price} 원 입니다.</p>
+    </div>
+  );
+}
+
+function Greet({ name, massage }) {
+  return (
+    <div>
+      <h3>{name} 님 에게</h3>
+      <p style={{ color: "red", fontSize: 30 }}>{massage}</p>
+    </div>
+  );
+}
+
+function VideoCard({ title, channel, views }) {
+  return (
+    <div>
+      <h3>{title}</h3>
+      <p>{channel}</p>
+      <p>조회수 : {views}</p>
+    </div>
+  );
+}
+
+function VideoList({ videos }) {
+  return (
+    <div>
+      {videos.map((video, index) => (
+        <VideoCard
+          key={index}
+          title={video.title}
+          channel={video.channel}
+          views={video.views}
+        />
+      ))}
+    </div>
+  );
+}
+
 function App() {
   const fruits = ['사과', '포도', '바나나'];
+  const videos = [
+    {
+      title: "최진서 tv",
+      channel: "최진서",
+      views: "1000만"
+    },
+    {
+      title: "태지환 tv",
+      channel: "태지환",
+      views: "2"
+    },
+  ];
+
   return (
     <div>
       <h1>4주차 실습</h1>
@@ -71,13 +130,22 @@ function App() {
       <Work num="4" />
       <Id name="최진서" age="23" job="학생" />
 
-      <Work num="5" />
-      <Button text="로그인" />
-      <Button text="회원가입" />
-      <Button text="삭제" />
+      <Work num="5" /> <p> 색 넣는거 추가</p>
+      <Button text="로그인" color="red" />
+      <Button text="회원가입" color="pink" />
+      <Button text="삭제" color="blue" />
 
       <h2>실습</h2>
       <FruitList fruits={fruits} />
+
+      <h2>실습 2</h2>
+      <Book title="파이썬" author="최진서" price={3000} />
+
+      <h2>실습 3</h2>
+      <Greet name="태지환" massage="예비군 갔다가 재입대 기원 뺑이쳐라" />
+
+      <h2>💸실습 4</h2>
+      <VideoList videos={videos} />
     </div>
   );
 }
