db6adce7a67b1302d5b1a651bf443b651be4d055
Jinseo Choi <choijinseo04@gmail.com>
2026-03-24 11:13:45 +0900

    0324추가

--- myapp_04/src/App.jsx
+++ myapp_04/src/App.jsx
@@ -1,22 +1,59 @@
-import './App.css'
+function Work({ num }) {
+  return <h3>실습 {num}</h3>;
+}
 
-function Profile({ name, age, job }) {
+function Hello() {
+  return <h1>안녕하세요 React</h1>;
+}
+
+function User({ name }) {
+  return <h2>안녕하세요 {name}님</h2>;
+}
+
+function Product({ name, price }) {
   return (
     <div>
-      <h2>{name}</h2>
-      <p>나이: {age}</p>
-      <p>직업: {job}</p>
+      <h4>상품 : {name}</h4>
+      <p>가격 : {price}</p>
     </div>
   );
+}
+
+function Id( { name, age, job }) {
+  return (
+    <div>
+      <p>이름 : {name}</p>
+      <p>나이 : {age}</p>
+      <p>직업 : {job}</p>
+    </div>
+  );
+}
+
+function Button({ text }) {
+  return <button>{text}</button>;
 }
 
 function App() {
   return (
     <div>
-      <h1>사용자 정보</h1>
-      <Profile name="김민지" age={20} job="학생" />
-      <Profile name="박준호" age={27} job="개발자" />
-      <Profile name="이서연" age={24} job="디자이너" />
+      <h1>4주차 실습</h1>
+      <hr />
+      <Work num="1" />
+      <Hello />
+
+      <Work num="2" />
+      <User name="진서" />
+      <User name="강준" />
+
+      <Work num="3" />
+      <Product name="노트북" price="100만원" />
+      <Product name="마우스" price="3만원" />
+
+      <Work num="4"/>
+      <Id name="최진서" age="23" job="학생"/>
+      
+      <Work num="5" />
+      <Button text="로그인" />
     </div>
   );
 }
